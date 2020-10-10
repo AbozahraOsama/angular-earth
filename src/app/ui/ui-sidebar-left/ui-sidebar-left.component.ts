94e3eb641e6980e8b23e1f81794211ca095a3efe
Osama Abozahra <abozahraosama@gmail.com>
2020-10-10 13:37:49 +0200

    Initial commit

---
+++ src/app/ui/ui-sidebar-left/ui-sidebar-left.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ui-sidebar-left',
+  templateUrl: './ui-sidebar-left.component.html'
+})
+export class UiSidebarLeftComponent implements OnInit {
+
+  public constructor() { }
+
+  public ngOnInit(): void {
+  }
+
+}
---
+++ src/app/ui/ui-sidebar-right/ui-sidebar-right.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ui-sidebar-right',
+  templateUrl: './ui-sidebar-right.component.html'
+})
+export class UiSidebarRightComponent implements OnInit {
+
+  public constructor() { }
+
+  public ngOnInit(): void {
+  }
+
+}
